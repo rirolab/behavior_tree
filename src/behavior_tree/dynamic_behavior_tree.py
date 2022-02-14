b04aa275f9c6a74611f82b740089889ca910afb0
Ulzhalgas Rakhman <11dulzhalgasr@gmail.com>
2022-02-14 13:49:01 +0900

    Update dynamic_behavior_tree.py

--- src/behavior_tree/dynamic_behavior_tree.py
+++ src/behavior_tree/dynamic_behavior_tree.py
@@ -368,6 +368,7 @@ if __name__ == '__main__':
                                                  'jobs.jog_job.Move',
                                                  'jobs.gripper_job.Move',
                                                  'jobs.slide_job.Move',
+                                                 'jobs.attach_job.Move',
                                                  'jobs.touch_job.Move'],
                                                  rec_topic_list=topic_list)
     rospy.on_shutdown(splintered_reality.shutdown)
