7dd8240c4aad039f71e6d4e56834746244662fa4
farawell <farawell777@kaist.ac.kr>
2024-06-01 03:22:01 +0900

    Initialized each class method of MOVEG() class

--- src/behavior_tree/subtrees/MoveWaypoint.py
+++ src/behavior_tree/subtrees/MoveWaypoint.py
@@ -31,18 +31,38 @@ class MOVEG(py_trees.behaviour.Behaviour):
     priority behaviour.
     """
 
-    def __init__(self):
+    def __init__(self, name):
+        # Initialization
+        super(MOVEG, self).__init__(name=name)
+        self.name = name
         self.goal = None
-        pass
-
+        
     def setup(self, timeout):
-        pass
+        rospy.loginfo(f"[Subtree] MOVEG : setup() called ({self.name}).")
+        self.logger.debug("{0}.setup()".format(self.__class__.__name__))
+
+        self.feedback_message = "{}: setup".format(self.name)
+
+        # ROS service
+        rospy.wait_for_service('/manage_map/get_region_goal')
+        self.getregiongoal_client = rospy.ServiceProxy('/manage_map/get_region_goal', getRegionGoal)
+
+        # ROS action client
+        self.nav_client = actionlib.SimpleActionClient("/move_base", MoveBaseAction)
+
+        return True
 
     def initialise(self):
-        pass
+        rospy.loginfo('[subtree] MOVEG: initialize() called.')
+        self.logger.debug("{0}.initialise()".format(self.__class__.__name__))
 
+        blackboard = py_trees.Blackboard()
+        self.goal = blackboard.get('goal')
+        
     def update(self):
-        pass
+        rospy.loginfo('[subtree] MOVEG: update() called.')
+        self.logger.debug("%s.update()" % self.__class__.__name__)
+        blackboard = py_trees.Blackboard()
 
     def terminate(self, new_status):
         pass
