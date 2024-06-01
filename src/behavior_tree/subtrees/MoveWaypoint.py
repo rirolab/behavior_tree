cdce1f04282195c82dd6bb7043c81df829161d8e
farawell <farawell777@kaist.ac.kr>
2024-06-01 14:12:24 +0900

    Added comments, partial implementation of terminate()

--- src/behavior_tree/subtrees/MoveWaypoint.py
+++ src/behavior_tree/subtrees/MoveWaypoint.py
@@ -36,8 +36,16 @@ class MOVEG(py_trees.behaviour.Behaviour):
         super(MOVEG, self).__init__(name=name)
         self.name = name
         self.goal = None
+
+    ############################################
+    # User Customisable Callbacks
+    ############################################
         
     def setup(self, timeout):
+        """
+        Set up and verify infrastructure (middleware connections, etc) is available.
+        """
+
         rospy.loginfo(f"[Subtree] MOVEG : setup() called ({self.name}).")
         self.logger.debug("{0}.setup()".format(self.__class__.__name__))
 
@@ -53,6 +61,10 @@ class MOVEG(py_trees.behaviour.Behaviour):
         return True
 
     def initialise(self):
+        """
+        Execute user specified instructions prior to commencement of a new round of activity.
+        """
+
         rospy.loginfo('[subtree] MOVEG: initialize() called.')
         self.logger.debug("{0}.initialise()".format(self.__class__.__name__))
 
@@ -60,9 +72,27 @@ class MOVEG(py_trees.behaviour.Behaviour):
         self.goal = blackboard.get('goal')
         
     def update(self):
+        """
+        Execute user specified instructions when the behaviour is ticked.
+        """
         rospy.loginfo('[subtree] MOVEG: update() called.')
         self.logger.debug("%s.update()" % self.__class__.__name__)
         blackboard = py_trees.Blackboard()
 
     def terminate(self, new_status):
-        pass
+        """
+        Execute user specified instructions when the behaviour is stopped.
+        """
+        # http://docs.ros.org/en/lunar/api/actionlib_msgs/html/msg/GoalStatus.html
+        # Checking the '/move_base' action client
+        result_state = self.nav_client.get_state()
+
+
+        # if result_state == GoalStatus.ACTIVE:
+        #     self.cmd_req( json.dumps({'action_type': 'cancel_goal'}) )
+        
+        self.logger.debug("%s.terminate()[%s->%s]" % (self.__class__.__name__, self.status, new_status))   
+        
+        blackboard = py_trees.Blackboard()
+        self.manip_status_update_req(blackboard.robot_name)
+        return 
