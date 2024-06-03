af0e1b99babc983a43ca0d5fd7f9ad6a6ee8c36a
farawell <farawell777@kaist.ac.kr>
2024-06-03 18:31:23 +0900

    Rough implementation of Goals2Blackboard.py, needs additional code lines

--- src/behavior_tree/subtrees/Goals2Blackboard.py
+++ src/behavior_tree/subtrees/Goals2Blackboard.py
@@ -11,14 +11,38 @@ import actionlib
 import std_msgs.msg as std_msgs
 from actionlib_msgs.msg import GoalStatus
 
-# Local imports
+# Local import
 from riro_navigation.msg import TaskPlanAction, TaskPlanResult, TaskPlanFeedback, GoalInfo, GoalsInfo
 from riro_navigation.msg import Dictlistfloat
 from riro_navigation.srv import LTLPlan, LTLPlanRequest
 
-class Goals2Blackboard:
-    def __init__(self):
-        pass
+class ToBlackboard(subscribers.ToBlackboard):
+
+    def __init__(self, name, topic_name="/task_goals"):
+        super(ToBlackboard, self).__init__(name=name,
+                                           topic_name = topic_name,
+                                           topic_type = std_msgs.String,
+                                           blackboard_variables = {"goals": None},
+                                           clearing_policy = py_trees.common.ClearingPolicy.NEVER
+                                           )
+
+        self.blackboard = py_trees.blackboard.Blackboard()
 
     def update(self):
-        pass
+        self.logger.debug("%s.update()" % self.__class__.__name__)
+        status = super(ToBlackboard, self).update()
+
+        # We've got new goals
+        if status != py_trees.common.Status.RUNNING:
+            # 'status' isn't RUNNING, but no goals are on the blackboard
+            if self.blackboard.grnd_msg.data is None:
+                rospy.logwarn_throttle(60, "%s: No goals on the blackboard!" % self.name)
+            
+            # Load the goals from the blackboard
+            goals = json.loads(self.blackboard.goals.data)
+        
+        # Create queue for goals
+        self.blackboard.queue = None
+        self.blackboard.queue = []
+        for goal in goals['goals']:
+            self.blackboard.queue.append(goal[1])
