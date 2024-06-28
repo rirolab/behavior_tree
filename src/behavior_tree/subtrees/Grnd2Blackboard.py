75b76696137a7e87927cb147989c4665867523bc
farawell <farawell777@kaist.ac.kr>
2024-06-28 13:36:39 +0900

    Edited Grnd2Blackboard.py to be aligned with world model

--- src/behavior_tree/subtrees/Grnd2Blackboard.py
+++ src/behavior_tree/subtrees/Grnd2Blackboard.py
@@ -1,9 +1,14 @@
-import py_trees
-import rospy
+# Standard imports
 import json
-import std_msgs.msg as std_msgs
+import queue
 
+# Third-party imports
+import py_trees
 from py_trees_ros import subscribers
+
+# ROS imports
+import rospy
+import std_msgs.msg as std_msgs
 
 class ToBlackboard(subscribers.ToBlackboard):
 
@@ -15,9 +20,16 @@ class ToBlackboard(subscribers.ToBlackboard):
                                            clearing_policy=py_trees.common.ClearingPolicy.ON_INITIALISE 
                                            )
 
+        # Blackboard initialization
         self.blackboard = py_trees.blackboard.Blackboard()
+
+        # stop command
         self.blackboard.stop_cmd = False
 
+        # Waypoint navigtation
+        # TODO: Implement world model for to replace goal_loc_dict
+        self.blackboard.goal_num = 0 # To track the number of goals
+        self.blackboard.list_goals = [] # To store and lookup the goals
 
     def update(self):
         self.logger.debug("%s.update()" % self.__class__.__name__)
@@ -40,7 +52,13 @@ class ToBlackboard(subscribers.ToBlackboard):
                 if primitive_action == "stop":
                     self.blackboard.stop_cmd = True
                     break
-                    
-            #self.feedback_message = "Grounding is ok"
+                elif primitive_action == "move_to_goal":
+                    self.blackboard.goal_num += 1
+                    self.blackboard.list_goals.append(grounding['params'][str(param_id+1)])
+                    break
+                else:
+                    rospy.logwarn_throttle(60, "%s: Unknown primitive action!" % self.name)
+                    self.blackboard.stop_cmd = True
+                    break
             
         return status
