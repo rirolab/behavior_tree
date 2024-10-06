2dc9fc0da11320ea2f42ef1a34e45a4ff2f8dd0f
farawell <farawell777@kaist.ac.kr>
2024-10-06 12:46:41 +0900

    WM2Blackboard.py now correctly saves the fed dictionary

--- src/behavior_tree/subtrees/WM2Blackboard.py
+++ src/behavior_tree/subtrees/WM2Blackboard.py
@@ -1,6 +1,7 @@
 import py_trees
 import rospy
 import std_msgs.msg as std_msgs
+import json
 
 from py_trees_ros import subscribers
 
@@ -15,7 +16,7 @@ class ToBlackboard(subscribers.ToBlackboard):
                                            )
 
         self.blackboard = py_trees.blackboard.Blackboard()
-
+        self.blackboard.wm_dict = {}
 
     def update(self):
         self.logger.debug("%s.update()" % self.__class__.__name__)
@@ -28,6 +29,14 @@ class ToBlackboard(subscribers.ToBlackboard):
             else:
                 self.blackboard.no_wm_warning=False
                 
-            self.feedback_message = "No world model " if self.blackboard.no_wm_warning else "World model is ok"
+                # Save the dictionary sent by the 'wm_msg' topic
+                self.blackboard.wm_msg = json.loads(self.blackboard.wm_msg.data)
+                
+                for i in self.blackboard.wm_msg['param_num']:
+                    self.blackboard.wm_dict[str[i + 1]] = \
+                        self.blackboard.wm_msg['worldmodel_dict'][str[i + 1]]
+                
+            self.feedback_message = "No world model " if self.blackboard.no_wm_warning \
+                                                      else "World model is ok"
             
         return status
