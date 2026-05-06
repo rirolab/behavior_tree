7372356dea53854396ee449bb22eb44ffc1f8632
YSK <yeseung.kim.88@gmail.com>
2026-05-06 20:49:13 +0900

    add arm/gripper goal status & id and full scenario complete

--- behavior_tree/multi_dynamic_behavior_tree.py
+++ behavior_tree/multi_dynamic_behavior_tree.py
@@ -46,21 +46,22 @@ def create_root(robot_names):
         topic_name="symbol_grounding",
     )
 
-    # Define goal_id and goal_status per robot in blackboard
+    # Define arm/gripper goal state per robot in blackboard.
     status_nodes = []
     for robot_name in robot_names:
-        status_nodes.append(
-            ToBlackboard(
-                name=f"{robot_name}_Status2BB",
-                topic_name=f"{robot_name}/arm_client/goal_status",
-                topic_type=GoalStatus,
-                blackboard_variables={
-                    f"{robot_name}/goal_id": "goal_info.goal_id.uuid",
-                    f"{robot_name}/goal_status": "status",
-                },
-                qos_profile=py_trees_ros.utilities.qos_profile_unlatched(),
+        for goal_channel in ["arm", "gripper"]:
+            status_nodes.append(
+                ToBlackboard(
+                    name=f"{robot_name}_{goal_channel}_Status2BB",
+                    topic_name=f"{robot_name}/arm_client/{goal_channel}/goal_status",
+                    topic_type=GoalStatus,
+                    blackboard_variables={
+                        f"{robot_name}/{goal_channel}/goal_id": "goal_info.goal_id.uuid",
+                        f"{robot_name}/{goal_channel}/goal_status": "status",
+                    },
+                    qos_profile=py_trees_ros.utilities.qos_profile_unlatched(),
+                )
             )
-        )
 
     priorities = py_trees.composites.Selector("Priorities", memory=False)
     priorities.add_child(py_trees.behaviours.Running(name="Idle"))
