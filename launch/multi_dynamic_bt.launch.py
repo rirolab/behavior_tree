6c9bcea38ab2f049dcac5c3371b40872174d9a77
Jun Park <jun.park@kaist.ac.kr>
2026-05-11 19:20:27 +0900

    Accept params_file LaunchArg in multi_dynamic_bt.launch.py

--- launch/multi_dynamic_bt.launch.py
+++ launch/multi_dynamic_bt.launch.py
@@ -3,6 +3,8 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
@@ -14,10 +16,12 @@ def generate_launch_description():
         :class:`~launch.LaunchDescription`: launch description for the tree node.
     """
     bt_dir = get_package_share_directory('behavior_tree')
-    params_file = os.path.join(
-        bt_dir,
-        'params',
-        'multi_default.yaml',
+    default_params = os.path.join(bt_dir, 'params', 'multi_default.yaml')
+
+    params_arg = DeclareLaunchArgument(
+        'params_file',
+        default_value=default_params,
+        description='YAML parameter file for the multi behavior tree node.',
     )
 
     bt_node = Node(
@@ -27,9 +31,10 @@ def generate_launch_description():
         output='screen',
         emulate_tty=True,
         arguments=[],
-        parameters=[params_file],
+        parameters=[LaunchConfiguration('params_file')],
     )
 
     return LaunchDescription([
+        params_arg,
         bt_node,
     ])
