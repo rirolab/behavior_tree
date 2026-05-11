c3a894d3b61130093fadda2eff6e128060b347c8
Jun Park <jun.park@kaist.ac.kr>
2026-05-11 19:20:13 +0900

    Drop DRB-specific artifacts and dead references

--- behavior_tree/multi_dynamic_behavior_tree.py
+++ behavior_tree/multi_dynamic_behavior_tree.py
@@ -451,10 +451,7 @@ def main(args=None):
             "jobs.move_job.Move",
             "jobs.gripper_job.Move",
             "jobs.policy_job.Move",
-            "jobs.dual_grasp_job.Move",
             "jobs.dual_move_job.Move",
-            # "jobs.dual_pick_policy_job.Move",
-            # "jobs.dual_place_policy_job.Move",
         ],
         rec_topic_list=topic_list,
     )
